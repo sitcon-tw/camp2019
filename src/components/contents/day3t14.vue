eb3053ef2edeafa448bb73b3f98fab20aad29c76
yaoandy107 <yaoandy107@gmail.com>
2019-08-08 19:03:19 +0800

    Update day3t14.vue

--- src/components/contents/day3t14.vue
+++ src/components/contents/day3t14.vue
@@ -16,8 +16,7 @@
       <template v-slot:content>
         <p>
           <a href="https://drive.google.com/open?id=1Cd12jOW74tjOFYGEhS6cnr5gbGJmiPdl">投影片</a><br />
-          共筆連結
-          <a href="https://hackmd.io/amX5HnbdSLC-JOFsOar2hg?both">https://hackmd.io/amX5HnbdSLC-JOFsOar2hg?both</a>
+          <a href="https://hackmd.io/amX5HnbdSLC-JOFsOar2hg?both">共筆</a>
         </p>
         <p>可能有人會聽過網路爬蟲這個詞，卻對牠沒有概念。牠是一類程式腳本的統稱，目的是自動從網路抓東西下來，為了取得需要的資料，比如文字，圖片，影音等。Python的原意是蠎蛇，蛇從虫部，在古代被視為廣義的蟲類，在現代則歸為爬蟲類，所以拿牠來寫強大的爬蟲也是很合理的。這堂課會教各位向一個網站請求資料，而要拿取回的資料做什麼應用，就由各位的創意和雙手完成，會很有成就感的。</p>
         <ul>
