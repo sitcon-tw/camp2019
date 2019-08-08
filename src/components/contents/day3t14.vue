a6c332a07fbe31f06e51377e6f1645ff6efe82a0
yaoandy107 <yaoandy107@gmail.com>
2019-08-08 10:33:57 +0000

    feat(day3t14): add slide and note links

--- src/components/contents/day3t14.vue
+++ src/components/contents/day3t14.vue
@@ -14,6 +14,11 @@
         <p>北科大資工系四年級，曾任北科程式設計研究社 1st 社長。平常喜歡不務正業的寫和學校無關的程式，偶爾寫一些小服務、開發一些 App、打打 CTF 的小廢廢。</p>
       </template>
       <template v-slot:content>
+        <p>
+          <a href="https://drive.google.com/open?id=1Cd12jOW74tjOFYGEhS6cnr5gbGJmiPdl">投影片</a><br />
+          共筆連結
+          <a href="https://hackmd.io/amX5HnbdSLC-JOFsOar2hg?both">https://hackmd.io/amX5HnbdSLC-JOFsOar2hg?both</a>
+        </p>
         <p>可能有人會聽過網路爬蟲這個詞，卻對牠沒有概念。牠是一類程式腳本的統稱，目的是自動從網路抓東西下來，為了取得需要的資料，比如文字，圖片，影音等。Python的原意是蠎蛇，蛇從虫部，在古代被視為廣義的蟲類，在現代則歸為爬蟲類，所以拿牠來寫強大的爬蟲也是很合理的。這堂課會教各位向一個網站請求資料，而要拿取回的資料做什麼應用，就由各位的創意和雙手完成，會很有成就感的。</p>
         <ul>
           <li>Telegram</li>
